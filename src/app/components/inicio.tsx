145fcb8774b27402b3d9ee94de871b95f4f9f899
Luciano <lucianomasuelli.lm@gmail.com>
2024-08-26 12:03:08 -0300

    feat: add mobile version for nosotros section and inicio section

--- src/app/components/inicio.tsx
+++ src/app/components/inicio.tsx
@@ -3,16 +3,23 @@ import { fustat, grotesk, ltInstitute } from "../fonts/fonts";
 
 export default function Inicio() {
   return (
-    <div className="flex flex-col self-center justify-center align-middle mt-20 overflow-hidden">
+    <div className="flex flex-col self-center justify-center align-middle sm:mt-20 mt-40 mx-10 overflow-hidden ">
       <Image
         src={"/logo2.png"}
         alt="logo"
         width={4000}
         height={880}
-        className="w-[60%] self-center"
+        className="w-[60%] min-w-[600px] self-center sm:block hidden"
+      />
+      <Image
+        src={"/logoMobile.png"}
+        alt="logo"
+        width={4000}
+        height={880}
+        className="h-[60%] self-center sm:hidden"
       />
       <div className={grotesk.className}>
-        <p className="w-[40%] text-gray-200 lg:text-3xl md:text-2xl sm:text-xl font-[400] ml-[15%] mt-28 ">
+        <p className="sm:w-[40%] w-full text-gray-200 lg:text-3xl md:text-2xl sm:text-xl font-[400] sm:ml-[15%] p-4 sm:mt-28 mt-10 sm:text-left text-justify ">
           Fusionando la naturaleza con el arte, creando espacios que inspiran,
           relajan y revitalizan a quienes los disfrutan.
         </p>
--- src/app/components/nosotros.tsx
+++ src/app/components/nosotros.tsx
@@ -1,17 +1,58 @@
+import Image from "next/image";
 import { grotesk } from "../fonts/fonts";
 import Pattern from "./bgPattern";
 
 export default function Nosotros() {
   return (
     <div className="h-screen overflow-y-visible">
-      <div className={grotesk.className}>
-        <h1 className="text-black lg:text-5xl md:text-4xl text-3xl font-light tracking-widest pt-40 ml-32 z-30 relative">
-          NOSOTROS
-        </h1>
+      <div
+        className={`${grotesk.className} min-[1700px]:mx-60 xl:mx-40 lg:mx-40 md:mx-28 sm:mx-20 mx-10`}
+      >
+        <div className="flex flex-col max-w-max pt-40 relative items-center">
+          <h1 className="text-black 2xl:text-5xl xl:text-5xl lg:text-4xl md:text-4xl text-3xl font-light tracking-[0.2em] z-30 ">
+            NOSOTROS
+          </h1>
+          <hr className="border-2 border-primaryGreen-500 divide-solid xl:w-32 w-20 mt-2"></hr>
+        </div>
+
+        <div className="flex flex-row align-top ">
+          <p className=" xl:w-[40rem] lg:w-96 md:w-80 sm:w-60 w-full xl:mt-10 mt-6 z-20 relative text-gray-700 xl:leading-8 xl:text-xl">
+            En el corazón de Mendoza, donde las montañas se encuentran con los
+            viñedos y la naturaleza despliega su majestuosidad, un grupo de
+            apasionados por el paisajismo y el diseño visualizó un sueño: crear
+            espacios verdes que no solo embellecieran el entorno, sino que
+            también ofrecieran un refugio para el alma. Así nació Oasis Care el
+            11 de octubre de 2023, con la misión de convertir cualquier espacio
+            en un oasis de tranquilidad y belleza.
+          </p>
+          <Image
+            src="/wpp5.jpeg"
+            alt="wpp2"
+            width={3024}
+            height={4032}
+            className="object-contain 2xl:w-[500px] xl:w-[400px] w-72 absolute z-20 sm:border-8 border-4 border-background mr-20 -mt-14 right-0 hidden sm:block"
+          />
+        </div>
+        <Image
+          src="/wpp3.jpeg"
+          alt="wpp3"
+          width={4030}
+          height={2725}
+          className="object-contain 2xl:w-[600px] xl:w-[500px]   w-96 relative z-20 mt-10 sm:border-8 border-4 border-background hidden sm:block"
+        />
+
+        {/*Mobile image*/}
+        <Image
+            src="/wpp5.jpeg"
+            alt="wpp2"
+            width={3024}
+            height={4032}
+            className="object-contain 2xl:w-[500px] xl:w-[400px] sm:w-72 sm:absolute z-20 sm:border-8 border-4 border-background sm:mr-20 sm:-mt-14 my-5 sm:right-0 sm:hidden relative"
+          />
       </div>
-      <div className="flex flex-row justify-between ">
-        <Pattern className="flex flex-row  justify-start -ml-36 mt-52 size-[30%] rotate-180 filter brightness-[70%] max-w-[350px] " />
-        <Pattern className="flex flex-row justify-end -mt-52 w-[60%]" />
+      <div className="flex flex-row justify-between sm:-mt-[600px] -mt-[600px] ">
+        <Pattern className="flex flex-row  justify-start sm:-ml-20 -ml-24  sm:mt-52 mt-40 sm:w-[30%] w-[45%] max-w-[500px] rotate-180 filter brightness-[70%]  absolute left-0 sm:opacity-100 opacity-90" />
+        <Pattern className="flex flex-row justify-end sm:-mt-52 -mt-80 pl-20 sm:w-[60%] w-[80%] absolute right-0 sm:opacity-80 opacity-30 " />
       </div>
     </div>
   );
