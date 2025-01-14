b2a918530b0f4746d1ca59267e4131dbc1d96f5d
Luciano <lucianomasuelli.lm@gmail.com>
2025-01-14 16:21:39 -0300

    feat: adjust styles on Nosotros section

--- src/app/components/nosotros.tsx
+++ src/app/components/nosotros.tsx
@@ -5,7 +5,10 @@ import SectionTitle from "./sectionTitle";
 
 export default function Nosotros() {
   return (
-    <div className="h-screen overflow-y-visible sm:mt-20 mt-10 sm:mb-20 " id="nosotros">
+    <div
+      className="h-fit overflow-y-visible sm:mt-20 mt-10 sm:mb-20 "
+      id="nosotros"
+    >
       <div
         className={`${grotesk.className} min-[1700px]:mx-80 xl:mx-60 lg:mx-40 md:mx-28 sm:mx-20 mx-10`}
       >
@@ -22,31 +25,44 @@ export default function Nosotros() {
             en un oasis de tranquilidad y belleza.
           </p>
           <Image
-            src="/wpp1.jpeg"
+            src="/nosotros1.jpg"
             alt="wpp2"
-            width={3024}
-            height={4032}
+            width={1000}
+            height={500}
             className="object-contain 2xl:w-[400px] xl:w-[300px] lg:w-[300px] w-72 absolute z-20 sm:border-8 border-4 border-background-500 min-[1700px]:mr-80 xl:mr-60 lg:mr-40 md:mr-28 sm:mr-20 mr-10 -mt-[60px] right-0 hidden sm:block rounded-tr-[50px] "
           />
         </div>
         <Image
-          src="/wpp3.jpeg"
+          src="/nosotros3.jpg"
           alt="wpp3"
-          width={4030}
-          height={2725}
-          className="object-cover 2xl:w-[600px] 2xl:h-[300px] xl:w-[600px] lg:w-[525px] md:w-[500px] sm:w-[300px] self-center justify-center w-80 relative z-20 mt-10 sm:border-8 border-4 border-background-500 hidden sm:block rounded-bl-[50px] "
+          width={500}
+          height={1000}
+          className="object-cover self-center justify-center w-[40vw] h-[50vh] relative z-20 mt-10 sm:border-8 border-4 border-background-500 hidden sm:block rounded-bl-[50px] "
         />
 
         {/*Mobile image*/}
-        <Image
+          <Image
+            src="/nosotros1.jpg"
+            alt="nos1"
+            width={1000}
+            height={500}
+            className="object-contain  z-20 border-4 border-background-500 my-5 relative rounded-tr-[50px] rounded-bl-[50px] sm:hidden "
+          />
+          <Image
+            src="/nosotros3.jpg"
+            alt="nos3"
+            width={500}
+            height={1000}
+            className="object-contain  z-20 border-4 border-background-500 my-5 relative rounded-tr-[50px] rounded-bl-[50px] sm:hidden "
+          />
+           {/* <Image
             src="/wpp1.JPG"
             alt="wpp2"
             width={3024}
             height={4032}
-            className="object-contain  z-20 border-4 border-background-500 my-5 sm:hidden relative rounded-tr-[50px] rounded-bl-[50px] "
-          />
+            className="object-contain  z-20 border-4 border-background-500 my-5 relative rounded-tr-[50px] rounded-bl-[50px] "
+          />  */}
       </div>
-      
     </div>
   );
 }
